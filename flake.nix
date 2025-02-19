6df04dbb139de578346f7af3a9c85241a24f04c7
kurogeek <panupong.jtp@gmail.com>
2025-02-19 13:38:59 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:flake.nix

--- flake.nix
+++ flake.nix
@@ -40,6 +40,8 @@
             hash = "sha256-iO8KVbAPYtlRl4FyaX51Wz/6Wt4GOxkESEGGrmGTGak=";
           };
           cargoDeps = prev.cargoDeps.overrideAttrs (lib.const {
+            name = "${name}-vendor";
+            inherit src;
             outputHash = "sha256-vJV3TV8XuILXeORkK7Xsj7GoFRpSiKCyKJ4zLBMWI7I=";
           });
         });
