2da0008775a4431f43dfa689e164601945ee616d
kurogeek <panupong.jtp@gmail.com>
2025-02-19 12:06:08 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:flake.nix

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
             owner = "prisma";
             repo = "prisma-engines";
             rev = version;
-            hash = "sha256-iO8KVbAPYtlRl4FyaX51Wz/6Wt4GOxkESEGGrmGTGak=";
+            hash = "";
           };
           cargoHash = "";
 
